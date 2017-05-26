2140d4006e9806d4be9b275a579772d244dcf890
chrrys <christian.rysgaard@gmail.com>
2017-05-26 22:10:47 +0200

    test

--- src/Position.java
+++ src/Position.java
@@ -10,6 +10,8 @@ public class Position {
     public Position(Position position, Speed speed) {
         this(position.getX() + speed.getDeltaX(), position.getY() + speed.getDeltaY());
         System.out.println("test");
+
+
     }
 
     @Override
